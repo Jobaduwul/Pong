e01e32cfc2d76cebfcdb7796cb5a8a6e036b77c2
Kazi Jawadul Islam Jishan <kazijawadulislam123@gmail.com>
2023-08-13 11:48:33 +0600

    player control script set up

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    float verticalInput;
+    public float speed;
+    public float yRange;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -yRange)
+        {
+            transform.position = new Vector2(transform.position.x, -yRange);
+        }
+        if (transform.position.y > yRange)
+        {
+            transform.position = new Vector2(transform.position.x, yRange);
+        }
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector2.up * speed * Time.deltaTime * verticalInput);
+    }
+}
